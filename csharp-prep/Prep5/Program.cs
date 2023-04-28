08433278afc3d86da545bb84ba0f4e285e1e8b14
jacob Todd <jacobtodd99@icloud.com>
2023-04-28 16:23:36 -0600

    prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,45 @@
 using System;
-
-class Program
+namespace Prep5
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            DisplayWelcome();
+
+            string userName = PromptUserName();
+            int userNumber = PromptUserNumber();
+
+            int squared = SquareNumber(userNumber);
+
+            DisplayResult(userName,squared);
+        }
+
+        static void DisplayWelcome()
+        {
+          Console.WriteLine("Welcome To the Program!");  
+        }
+        static string PromptUserName()
+        {
+            Console.Write("please enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine()); // why do I need to use "int.Parse" for integers?
+            return number;
+        }
+        static int SquareNumber(int number)
+        {
+            int square = number * number ;
+            return square;
+        }
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
     }
 }
