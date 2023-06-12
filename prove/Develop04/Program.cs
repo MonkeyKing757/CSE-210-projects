63a11bea34c124be6954f6ee8bf82458baddf897
jacob Todd <jacobtodd99@icloud.com>
2023-06-12 14:10:35 -0600

    correction on last mistake

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -6,14 +6,7 @@ namespace Develop04
     {
         static void Main(string[] args)
         {
-            
 
-            Scripture s = new Scripture();
-            // Scripture b = new Scripture("Matt 1:9");
-            
-            s.SpitOutReferenceText();
-            s.VerseSpit();
-            // b.SpitOutReferenceText();
         }
     }
 }
