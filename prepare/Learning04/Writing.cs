eedf7fb0c735c53852e7eeaf675e69385d252d40
jacob Todd <jacobtodd99@icloud.com>
2023-07-07 16:17:41 -0600

    Inheritance

---
+++ prepare/Learning04/Writing.cs
@@ -0,0 +1,21 @@
+using System;
+namespace Learning04
+{
+    public class Writing : Assignment
+    {
+        private string _title;
+
+        public Writing(string studentName, string topic, string title)
+            : base(studentName, topic)
+        {
+            _title = title;
+        }
+
+        public string GetWritingInformation()
+        {
+            string studentName = GetStudentName();
+
+            return $"{_title} by {studentName}";
+        }
+    }
+}
